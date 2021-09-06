d5cf194d20a3cb073eede01aa18a8fbb4f28d645
Bryan Taylor <mcbtay@gmail.com>
2021-09-06 11:36:48 -0400

    importing TodosList, updating render to render the todos list

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import TodosList from "./TodosList"
+
 class TodoContainer extends React.Component {
   state = {
     todos: [
@@ -23,11 +25,9 @@ class TodoContainer extends React.Component {
     
   render() {
     return (
-      <ul>
-        {this.state.todos.map(todo => (
-          <li>{todo.title}</li>
-        ))}
-      </ul>
+      <div>
+        <TodosList todos={this.state.todos} />
+      </div>
     );
   }
 }
