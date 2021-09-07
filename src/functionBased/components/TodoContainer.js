c3f20ae01b2868c7b0a47aa61d94da34b15f1e58
Bryan Taylor <mcbtay@gmail.com>
2021-09-06 21:48:11 -0400

    adding switch and other routes for about and 404

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react"
 import { v4 as uuidv4 } from "uuid"
 import { Route, Switch } from "react-router-dom"
 
+import About from "../pages/About"
+import NotMatch from "../pages/NotMatch"
+
 import Header from "./Header"
 import InputTodo from "./InputTodo"
 import TodosList from "./TodosList"
@@ -72,20 +75,28 @@ const TodoContainer = () => {
   }, [todos])
     
   return (
-    <Route exact path="/">
-      <div className="container">
-        <div className="inner">
-          <Header />
-          <InputTodo addTodoProps={addTodo} />
-          <TodosList 
-            todos={todos}
-            handleChangeProps={handleChange}
-            deleteTodoProps={deleteTodo} 
-            updateTodoProps={updateTodo}
-          />
+    <Switch>
+      <Route exact path="/">
+        <div className="container">
+          <div className="inner">
+            <Header />
+            <InputTodo addTodoProps={addTodo} />
+            <TodosList 
+              todos={todos}
+              handleChangeProps={handleChange}
+              deleteTodoProps={deleteTodo} 
+              updateTodoProps={updateTodo}
+            />
+          </div>
         </div>
-      </div>
-    </Route>
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="*">
+        <NotMatch />
+      </Route>
+    </Switch>
   );
 }
 
