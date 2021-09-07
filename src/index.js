61fb199d2407269f2252cb434d8f2f34a7e972b7
Bryan Taylor <mcbtay@gmail.com>
2021-09-06 22:50:49 -0400

    using hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 import TodoContainer from './functionBased/components/TodoContainer'
 
@@ -8,8 +8,8 @@ import "./functionBased/App.css"
 
 ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <TodoContainer />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
     document.getElementById("root"));
